110e96e84358e161c02d0e2272f80b3bbdcf15c1
Bryce Dumas <bryce.dumas@gmail.com>
2020-04-21 12:29:02 +0000

    Automatically backed up by Learn

---
+++ lib/song.rb
@@ -0,0 +1,19 @@
+class Song 
+  attr_accessor :name, :artist, :genre 
+  @@count = 0 
+  @@genres = []
+  
+  def initialize (genre)
+    @@count += 1
+    @@genres << genre
+  end
+  def count
+    @@count
+  end
+  def genres
+    unique_genres = []
+    @@genres.each do |genre|
+      unique_genres << genre unless unique_genres.include?(genre)
+    end
+  end
+end
